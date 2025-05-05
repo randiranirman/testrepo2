9669d9de62840c68bd42dab665f6601884717a6b
Randira nirman <randiranirman20011210@gmail.com>
2025-05-05 23:02:56 +0530

    created the user class

---
+++ src/main/java/org/example/testtoyproject/UUser.java
@@ -0,0 +1,16 @@
+package org.example.testtoyproject;
+
+public class UUser {
+
+
+    private String username;
+    private String  name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
